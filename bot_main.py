f77a448f823de105b69c52ba56b36a89e13a2fc2
AJMaynard89 <AJMaynard89@gmail.com>
2018-03-03 11:08:12 -0500

    Fixed compatability with newest version of discord.py

--- bot_main.py
+++ bot_main.py
@@ -1,65 +1,69 @@
 from discord.ext.commands import Bot
 import subprocess as sp
 import shodan
-
+import asyncio
 
 bot = Bot(command_prefix="$")
-token = <ENTER DISCORD API KEY>
+token = 'INSERT KEY HERE' #THIS SHOULD PROBABLY JUST READ FROM A CONFIG FILE
 searchTerm = str()
 numResults = int()
 
-@bot.event
-async def on_ready():
+@bot.async_event
+def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
 
-@bot.event
-async def on_message(message):
-    if message.content.startswith(('why', 'Why')):
-        await bot.send_file(message.channel, "images/bytyan.jpg")
-    elif message.content.startswith(('lol', 'LOL', 'Lol')):
-        await bot.send_message(message.channel, content=":laughing:")
-    await bot.process_commands(message)
+@bot.async_event
+def on_message(message):
+    if message.content.upper().startswith(('WHY ')):
+        yield from bot.send_file(message.channel, "images/bytyan.jpg")
+    elif message.content.upper().startswith(('LOL ', 'HAHA ','ROFL ')):
+        yield from bot.send_message(message.channel, content=":laughing:")
+    elif 'CHEF' in message.content.upper():
+        yield from bot.send_file(message.channel, "images/chef.jpg")
+    yield from bot.process_commands(message)
 
 
-@bot.command()
-async def log():
-    await bot.logout()
+@bot.command
+@asyncio.coroutine
+def log():
+    yield from bot.logout()
+
+@bot.command
+@asyncio.coroutine
+def r2d2():
+    yield from bot.say("https://www.youtube.com/watch?v=Uj1ykZWtPYI")
 
 
-@bot.command()
-async def r2d2():
-    await bot.say("https://www.youtube.com/watch?v=Uj1ykZWtPYI")
+@bot.command(name = 'sayshit')
+@asyncio.coroutine
+def sayshit():
+    yield from bot.say("Shit!")
+
+@bot.command
+@asyncio.coroutine
+def clear():
+    yield from bot.upload("images/project.jpg")
+    yield from bot.say("CLEARING")
+    yield from bot.upload("images/project.jpg")
+    yield from bot.say("CLEARING")
+    yield from bot.upload("images/project.jpg")
+    yield from bot.say("CLEARING")
+    yield from bot.upload("images/project.jpg")
 
 
-@bot.command()
-async def sayshit():
-    await bot.say("Shit!")
-
-
-    
-@bot.command()
-async def clear():
-    await bot.upload("images/project.jpg")
-    await bot.say("CLEARING")
-    await bot.upload("images/project.jpg")
-    await bot.say("CLEARING")
-    await bot.upload("images/project.jpg")
-    await bot.say("CLEARING")
-    await bot.upload("images/project.jpg")
-
-
-@bot.command()
-async def shodansearch(searchTerm):
-    SHODAN_API_KEY = <ENTER SHODAN KEY>
+@bot.command
+@asyncio.coroutine
+def shodansearch(searchTerm):
+    SHODAN_API_KEY = 'INSERT KEY HERE' #THIS SHOULD PROBABLY JUST READ FROM A CONFIG FILE
     api = shodan.Shodan(SHODAN_API_KEY)
     results = api.search(searchTerm)
     i = 0
 
-    await bot.upload("images/logo.png")
+    yield from bot.upload("images/logo.png")
 
     for result in results['matches'][:5]:
         i = i + 1
@@ -68,11 +72,11 @@ async def shodansearch(searchTerm):
         hostnames = result['hostnames']
         hostinfo = api.host(result['ip_str'])
 
-        await bot.say("Result %s of 5" % i)
-        await bot.say("IPinfo: %s" % ipInfo)
-        await bot.say("Hostname: %s" % hostnames)
-        await bot.say("OS: %s" % osData)
-        await bot.say(("Open Ports: %s" % str(hostinfo['ports'])))
-        await bot.say("-------------\n")
+        yield from bot.say("Result %s of 5" % i)
+        yield from bot.say("IPinfo: %s" % ipInfo)
+        yield from bot.say("Hostname: %s" % hostnames)
+        yield from bot.say("OS: %s" % osData)
+        yield from bot.say(("Open Ports: %s" % str(hostinfo['ports'])))
+        yield from bot.say("-------------\n")
 
 bot.run(token)
