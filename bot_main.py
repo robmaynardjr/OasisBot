3b7892066fe728bef9a0531b65ec21d45a53e285
AJMaynard89 <AJMaynard89@gmail.com>
2018-03-04 23:12:03 -0500

    added meme command

--- bot_main.py
+++ bot_main.py
@@ -3,12 +3,14 @@ from configparser import SafeConfigParser
 import codecs
 import subprocess as sp
 import shodan
+import flippy
 import asyncio #probably unnecessary
 
 parser = SafeConfigParser()
+f = flippy.Flippy()
 with codecs.open('./config.cfg', 'r', encoding='utf-8') as conf:
     parser.readfp(conf)
-    
+
 bot = Bot(command_prefix="$")
 token = parser.get('AUTH', 'discord')
 searchTerm = str()
@@ -30,6 +32,18 @@ def on_message(message):
     elif 'CHEF' in message.content.upper():
         yield from bot.send_file(message.channel, "images/chef.jpg")
     yield from bot.process_commands(message)
+
+
+@bot.command(name='meme')
+@asyncio.coroutine
+def makeMeme(template, text0, text1):
+    imgflipUser = parser.get('AUTH', 'imgflipUser')
+    imgflipPass = parser.get('AUTH', 'imgflipPass')
+    memeIds = {'mocking':102156234, 'doesnotsimply':61579, 'leo':5496396}
+    meme = f.genMeme(memeIds[template], imgflipUser, imgflipPass, text0, text1)
+    yield from bot.say(meme)
+    #genMeme(template_id, username, password, text0, text1, font="impact"):
+
 
 
 @bot.command(name='log')
@@ -77,6 +91,6 @@ def shodansearch(searchTerm):
         hostnames = result['hostnames']
         hostinfo = api.host(result['ip_str'])
 
-        yield from bot.say("Result %s of 5\nIPinfo: %s\nHostname: %s\nOS: %s\nOpen Ports: %s\n-------------\n" % (i, ipInfo, hostnames, osData, (str(hostinfo['ports']))))        
+        yield from bot.say("Result %s of 5\nIPinfo: %s\nHostname: %s\nOS: %s\nOpen Ports: %s\n-------------\n" % (i, ipInfo, hostnames, osData, (str(hostinfo['ports']))))
 
 bot.run(token)
