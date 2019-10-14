d54ba18aaecb697f2c6692571579ec81f7eaebbf
robmaynardjr <robmaynardjr@gmail.com>
2019-10-13 20:26:18 -0400

    Remove unnecessary images; troubleshoot clear

--- bot_main.py
+++ bot_main.py
@@ -38,11 +38,11 @@ async def makeMeme(oasisbot, template, text0, text1):
 
 @oasisbot.command()
 async def clear(oasisbot):
-    await oasisbot.send(file='images/project.jpg')
+    await oasisbot.send(file=discord.File('images/project.jpg'))
     await oasisbot.send("CLEARING")
-    await oasisbot.send(file='images/project.jpg')
+    await oasisbot.send(file=discord.File('images/project.jpg'))
     await oasisbot.send("CLEARING")
-    await oasisbot.send(file='images/project.jpg')
+    await oasisbot.send(file=discord.File('images/project.jpg'))
     await oasisbot.send("CLEARING")
 
 @oasisbot.command()
